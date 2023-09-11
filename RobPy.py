50c82c42ddfe40d47e37b7023474c0dd635fe0f1
Guilherme Andrade <42524262+guiandradedev@users.noreply.github.com>
2023-09-11 14:54:32 -0300

    feat: update robpy

--- RobPy.py
+++ RobPy.py
@@ -11,7 +11,10 @@ def cria_vetor3(vlist: list) -> np.ndarray:
     :param vlist: Lista com as componentes [vx, vy, vz] do vetor desejado
     :return: np.ndarray: vetor (3, 1) com os valores desejados
     """
-    pass
+
+    if len(vlist) != 3:
+        raise ValueError('A lista deveria ter 3 posicoes')
+    return np.asarray([vlist]).T
 
 
 def checa_vetor3(v: np.ndarray) -> None:
@@ -20,7 +23,8 @@ def checa_vetor3(v: np.ndarray) -> None:
     :param v:
     :return:
     """
-    pass
+    if v.shape != (3,1):
+        raise ValueError('O vetor deveria ser [3,1]')
 
 
 def produto_escalar(v1: np.ndarray, v2: np.ndarray) -> float:
@@ -30,7 +34,14 @@ def produto_escalar(v1: np.ndarray, v2: np.ndarray) -> float:
     :param v2: vetor (np.ndarray) coluna de 3 elementos
     :return: escalar: resultado de v1.v2
     """
-    pass
+    checa_vetor3(v1)
+    checa_vetor3(v2)
+
+    # caso não seja retorna erro, ou seja, nem passa por aqui!
+    """aux = v1.T @ v2
+    return float(aux[0][0])"""
+
+    return float((v1.T @ v2)[0][0])
 
 
 def norma_vetor(v: np.ndarray) -> float:
@@ -39,7 +50,7 @@ def norma_vetor(v: np.ndarray) -> float:
     :param v: vetor (np.ndarray) coluna de 3 elementos
     :return: escalar: norma do vetor
     """
-    pass
+    return np.sqrt(produto_escalar(v, v))
 
 
 def tamanho_proj_vetores(v1: np.ndarray, v2: np.ndarray) -> float:
@@ -59,7 +70,7 @@ def proj_vetores(v1: np.ndarray, v2: np.ndarray) -> np.ndarray:
     :param v2: vetor (np.ndarray) coluna de 3 elementos
     :return: vetor (np.ndarray) coluna de 3 elementos com o resultado da projeção
     """
-    pass
+    return (produto_escalar(v1, v2) / produto_escalar(v2, v2)) * v2
 
 
 def ang_vetores(v1: np.ndarray, v2: np.ndarray) -> np.ndarray:
@@ -69,8 +80,7 @@ def ang_vetores(v1: np.ndarray, v2: np.ndarray) -> np.ndarray:
     :param v2: vetor (np.ndarray) coluna de 3 elementos
     :return: escalar: ângulo em radianos
     """
-    pass
-
+    return np.arccos(produto_escalar(v1, v2)/(norma_vetor(v1) * norma_vetor(v2)))
 
 def produto_vetorial(v1: np.ndarray, v2: np.ndarray) -> np.ndarray:
     """
